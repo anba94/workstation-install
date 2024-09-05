bb8bcd605540898dc338336d2e8881fec2464b70
Andre <7537827+anba94@users.noreply.github.com>
2024-09-05 19:07:57 +0200

    feat: added first_run script

---
+++ first_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p git/priv git/others
+
+git clone https://github.com/anba94/workstation-install.git git/priv
+
+sudo dnf install ansible
+
+echo "Running Ansible playbook 1..."
+ansible-playbook git/priv/workstation-install/ansible/packages.yaml --become
+
+echo "Running Ansible playbook 2..."
+ansible-playbook git/priv/workstation-install/ansible/fonts.yaml --become
+
+ansible-playbook git/priv/workstation-install/ansible/flatpaks.yaml --become
+
+# ansible-playbook git/priv/workstation-install/ansible/rice.yaml --become
+
+echo "Script execution completed."
