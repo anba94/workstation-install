688e1ddb40c1935c1a1dff32f50420d4ab5ddc1a
André <anba94+github@gmx.net>
2024-09-05 21:41:07 +0200

    chore: changes yes

--- first_run.sh
+++ first_run.sh
@@ -4,7 +4,7 @@ mkdir -p git/priv git/others
 
 git clone https://github.com/anba94/workstation-install.git git/priv
 
-sudo dnf install ansible -y
+sudo dnf install -y ansible
 
 echo "Running Ansible playbook 1..."
 ansible-playbook git/priv/workstation-install/ansible/packages.yaml --become
